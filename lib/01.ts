d193f212a93b053c0b435b6b9dd895ed380fe81b
kyomusyugi <k0esij@naver.com>
2025-09-25 22:32:30 +0900

    Add files via upload

---
+++ lib/01.ts
@@ -0,0 +1,38 @@
+ // lib/01.ts
+
+export type Word = {
+  kanji: string;
+  kun: string;
+  on: string;
+};
+
+export const words_01: Word[] = [
+  { kanji: "歌", kun: "노래", on: "가" },
+  { kanji: "家", kun: "집", on: "가" },
+  { kanji: "加", kun: "더할", on: "가" },
+  { kanji: "街", kun: "거리", on: "가" },
+  { kanji: "価", kun: "값", on: "가" },
+  { kanji: "可", kun: "허락할", on: "가" },
+  { kanji: "仮", kun: "거짓", on: "가" },
+  { kanji: "佳", kun: "아름다울", on: "가" },
+  { kanji: "苛", kun: "가혹할", on: "가" },
+  { kanji: "架", kun: "가설할", on: "가" },
+  { kanji: "嫁", kun: "시집갈", on: "가" },
+  { kanji: "暇", kun: "틈, 한가할", on: "가" },
+  { kanji: "稼", kun: "일할", on: "가" },
+  { kanji: "角", kun: "뿔, 모퉁이", on: "각" },
+  { kanji: "各", kun: "각각", on: "각" },
+  { kanji: "覚", kun: "깨달을", on: "각" },
+  { kanji: "刻", kun: "새길", on: "각" },
+  { kanji: "閣", kun: "관서", on: "각" },
+  { kanji: "却", kun: "물리칠", on: "각" },
+  { kanji: "殻", kun: "껍질", on: "각" },
+  { kanji: "脚", kun: "다리", on: "각" },
+  { kanji: "間", kun: "사이", on: "간" },
+  { kanji: "刊", kun: "새길", on: "간" },
+  { kanji: "幹", kun: "줄기", on: "간" },
+  { kanji: "干", kun: "간여할, 마를", on: "간, 건" },
+  { kanji: "看", kun: "볼", on: "간" },
+  { kanji: "簡", kun: "간략할", on: "간" },
+  { kanji: "肝", kun: "간, 요긴할", on: "간" },
+];
